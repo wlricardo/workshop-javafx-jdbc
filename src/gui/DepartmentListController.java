f85f2140516d09ee15f9a93739e7ab4a135a15b1
wlricardo <wlricardo@gmail.com>
2022-02-20 13:13:21 -0300

    DepartmentList controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,48 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable {
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+
+	@FXML
+	private Button btnNew;
+
+	@FXML
+	public void onBtnNewAction() {
+		System.out.println("Buttom activated !!");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+}
